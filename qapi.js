dec84d31c4a00be18eddb54c8251cae1ccc343f4
Alexander <webisforce@gmail.com>
2020-10-08 20:48:42 +0300

    Some changes

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f7f03303d84d4001ec38366",
+				"5f7f03303d84d4001ec38368"
+			]
+		},
+		"5f7f03303d84d4001ec38366": {
+			"id": "5f7f03303d84d4001ec38366",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f7f03303d84d4001ec38368": {
+			"id": "5f7f03303d84d4001ec38368",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,20 @@ export default (() => {
 		</Helmet>
 		<Section padding="100px 0" sm-padding="40px 0">
 			<Override slot="SectionContent" align-items="center" />
-			<Link href="https://google.com">
+			<Link href="https://google.comddd">
 				Some text
 			</Link>
 			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
 				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
 			</Text>
 			<Text as="p" font="--lead" margin="20px 0 0 0">
 				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
